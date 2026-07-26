fe2fca37d79d521d730e5ad97ac145cc8c14572a
SD-Singh69 <sehajdeep2649@gmail.com>
2026-07-26 21:49:07 +0530

    Convert QueueEntry model to ES modules

--- server/src/models/QueueEntry.js
+++ server/src/models/QueueEntry.js
@@ -1,45 +1,45 @@
-const mongoose = require('mongoose');
+import mongoose from "mongoose";
 
 const queueEntrySchema = new mongoose.Schema({
   shopId: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'Shop',
-    required: true
+    ref: "Shop",
+    required: true,
   },
   customerName: {
     type: String,
     required: true,
-    trim: true
+    trim: true,
   },
   customerPhone: {
     type: String,
     trim: true,
-    default: ''
+    default: "",
   },
   tokenNumber: {
     type: Number,
-    required: true
+    required: true,
   },
   status: {
     type: String,
-    enum: ['waiting', 'serving', 'served', 'cancelled'],
-    default: 'waiting'
+    enum: ["waiting", "serving", "served", "cancelled"],
+    default: "waiting",
   },
   notifiedPosition3: {
     type: Boolean,
-    default: false
+    default: false,
   },
   notifiedTurn: {
     type: Boolean,
-    default: false
+    default: false,
   },
   servedAt: {
-    type: Date
+    type: Date,
   },
   createdAt: {
     type: Date,
-    default: Date.now
-  }
+    default: Date.now,
+  },
 });
 
-module.exports = mongoose.model('QueueEntry', queueEntrySchema);
+export default mongoose.model("QueueEntry", queueEntrySchema);
