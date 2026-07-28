e0d4fe482800225cb93fe2fd44a6be05dcb5d031
SD-Singh69 <sehajdeep2649@gmail.com>
2026-07-28 22:31:34 +0530

    Fix 3

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const client = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5002/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
 });
 client.interceptors.request.use((c) => {
   const s = JSON.parse(localStorage.getItem("ql_session") || "{}");
